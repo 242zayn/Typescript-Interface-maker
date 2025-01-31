457e6ab1add1b2fe834f7ffbfa2e7a294fa5eea8
242zayn <sarveshsharma@gmail.com>
2025-01-31 19:44:46 +0530

    Commit before run build

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,26 +1,7 @@
-import { DialogDemo } from "@/components/DialogDemo";
-import Joblist from "@/components/Joblist";
 import React from "react";
 
 const page = () => {
-  return (
-    <div className="w-full   antialiased pt-10">
-      <div className=" max-w-[90%] m-auto  ">
-        <div className="max-w-5xl mx-auto p-4">
-          <h1 className="relative z-10 text-lg md:text-5xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-            Generate your Data Structure in{" "}
-            <span className=" text-sky-600 dark:text-blue-500">Interface</span>{" "}
-          </h1>
-        </div>
-        <DialogDemo />
-        <div className="w-full py-3">
-          <div>
-            <Joblist />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>page</div>;
 };
 
 export default page;
